76fe0da85475dcca49ab903fc8ba86a96f89a31e
Nace Logar <nace.logar@3fs.si>
2017-06-15 09:14:16 +0200

    Add some typescript rules

--- configs/ts.js
+++ configs/ts.js
@@ -3,5 +3,15 @@ module.exports = {
     'parser': 'typescript-eslint-parser',
     'plugins': [
         'typescript'
-    ]
+    ],
+    'rules': {
+        'typescript/type-annotation-spacing': 'error',
+        'typescript/explicit-member-accessibility': 'off',
+        'typescript/interface-name-prefix': ['error', 'never'],
+        'typescript/no-triple-slash-reference': 'error',
+        'typescript/no-explicit-any': 'off',
+        'typescript/no-angle-bracket-type-assertion': 'error',
+        'typescript/no-namespace': 'off',
+        'typescript/prefer-namespace-keyword': 'error'
+    }
 };
