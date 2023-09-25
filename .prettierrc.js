33f40a7242169a6865400c9674eaa01d229c8785
Matej Zvan <matej.zvan@3fs.si>
2023-09-25 10:31:24 +0200

    Add prettier and remove unneeded rules
    Run: npx eslint-config-prettier configs/react-ts.js and remove what's
    reported from package eslint
    
    remove; unnecessary might conflict:
    - array-bracket-newline
    - array-element-newline
    - arrow-parens
    - arrow-spacing
    - block-spacing
    - brace-style
    - comma-dangle
    - comma-spacing
    - comma-style
    - computed-property-spacing
    - dot-location
    - eol-last
    - func-call-spacing
    - function-paren-newline
    - generator-star-spacing
    - implicit-arrow-linebreak
    - indent
    - key-spacing
    - keyword-spacing
    - max-statements-per-line
    - new-parens
    - newline-per-chained-call
    - no-extra-parens
    - no-extra-semi
    - no-floating-decimal
    - no-mixed-spaces-and-tabs
    - no-multi-spaces
    - no-multiple-empty-lines
    - no-trailing-spaces
    - no-whitespace-before-property
    - object-curly-newline
    - object-curly-spacing
    - object-property-newline
    - operator-linebreak
    - padded-blocks
    - quote-props
    - rest-spread-spacing
    - semi
    - semi-spacing
    - semi-style
    - space-before-blocks
    - space-before-function-paren
    - space-in-parens
    - space-infix-ops
    - space-unary-ops
    - switch-colon-spacing
    - template-curly-spacing
    - template-tag-spacing
    - wrap-iife
    - yield-star-spacing
    
    Removed; Enabled might conflict:
    - curly
    - no-tabs
    
    Leave; can not check:
    - max-len
    - no-mixed-operators
    - no-unexpected-multiline
    - quotes

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  useTabs: false,
+  tabWidth: 2,
+  singleQuote: true,
+  printWidth: 100,
+  semi: true,
+  bracketSpacing: true,
+  trailingComma: 'all',
+  endOfLine: 'lf',
+};
